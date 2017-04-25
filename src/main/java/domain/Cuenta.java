3319722b60abdc6fb0eb12277bdacff1a3c3e501
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-04-25 14:10:33 -0300

    El buen eclipse que no me guarda los cambios

--- src/main/java/domain/Cuenta.java
+++ src/main/java/domain/Cuenta.java
@@ -3,6 +3,28 @@ package domain;
 import org.joda.time.LocalDate;
 
 public class Cuenta {
-	private LocalDate fecha;
-	private int valor;
+	private String nombre;
+	private LocalDate periodo;
+	private int balance;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public LocalDate getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(LocalDate periodo) {
+		this.periodo = periodo;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
 }
