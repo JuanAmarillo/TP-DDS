eeefdc5d0f0650dd92988be3e93529e751a3a363
JuanAmarillo <juan.alb.amarillo@gmail.com>
2017-04-25 19:09:20 -0300

    Repositorio Empresas

---
+++ src/main/java/domain/repositorios/RepositorioEmpresas.java
@@ -0,0 +1,11 @@
+package domain.repositorios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Empresa;
+
+public class RepositorioEmpresas {
+	public static List<Empresa> empresasCargadas = new ArrayList<Empresa>();
+	
+}
