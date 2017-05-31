24dffe638343e66df2aee46b148207babd3f68d5
LPapazian <lilapapazian@msn.com>
2017-05-31 17:36:04 -0300

    Una idea para un FileLoader más genérico

--- src/main/java/interfaces/FileLoader.java
+++ src/main/java/interfaces/FileLoader.java
@@ -3,5 +3,15 @@ package interfaces;
 import java.io.IOException;
 
 public interface FileLoader<T> {
-	public void cargarArchivo(T lugarDeCarga) throws IOException;
+	public void cargarArchivo(String lugarDeCarga) throws IOException;
+	/* Si se pudiese armar un repo mas genérico, se podría armar
+	 * T aDevolver = getElementoDelArchivo(lugarDeCarga);
+	 * -->se delega al repositorio correspondiente
+	 */
+	/*public T getElementoDelArchivo(String lugarDeCarga) throws IOException{
+	 * ObjectMapper mapper = new ObjectMapper();
+	 * T aDevolver = mapper.readValue(new File(lugarDeCarga), T.class)
+	 * return aDevolver;
+	 * 
+	 */
 }
