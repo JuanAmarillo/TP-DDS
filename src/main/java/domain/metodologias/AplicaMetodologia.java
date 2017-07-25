b4e1be5738feace5944c3de7e0aa2ab62eae02a3
padillajuan <juanpadilla.jip@gmail.com>
2017-07-25 08:27:24 -0300

    Ordeno la clase

--- src/main/java/domain/metodologias/AplicaMetodologia.java
+++ src/main/java/domain/metodologias/AplicaMetodologia.java
@@ -29,14 +29,13 @@ public class AplicaMetodologia {
 
 	private void ordenarLista() {
 		listaEmpresas = listaEmpresas.stream()
-					   .sorted((e1,e2) -> (int) compararValores(e1,e2))
+					   .sorted((e1,e2) -> compararValores(e1,e2))
 					   .collect(Collectors.toList());
 	}
 
 	private int compararValores(EmpresaEnCalculo e1, EmpresaEnCalculo e2) {
 		return Double.compare(e2.getPesoAcumulado(),e1.getPesoAcumulado());
 	}
-
 
 	private void aplicarTaxativas(List<CondicionTaxativa> condicionesTaxativas, String periodo) {
 		condicionesTaxativas.stream().forEach(cond -> aplicarUnicaTaxativa(cond, periodo));
@@ -46,30 +45,17 @@ public class AplicaMetodologia {
 		actualizarLista(cond.aplicarCondicion(obtenerLista(), periodo));
 	}
 
-	private void actualizarLista(List<Empresa> listaFiltrada) {
-		List<EmpresaEnCalculo> empresasEnCalculo = generarEmpresasEnCalculo(listaFiltrada);
-		listaEmpresas = listaEmpresas.stream()
-					 .filter(empresa -> contieneALaEmpresa(empresa.getEmpresa(), empresasEnCalculo)).collect(Collectors.toList());
-	}
-
-	private boolean contieneALaEmpresa(Empresa empresa, List<EmpresaEnCalculo> empresasEnCalculo) {
-		return empresasEnCalculo.stream().map(empresaMap -> empresaMap.getEmpresa()).anyMatch(empresaFind -> empresaFind.esLaMismaEmpresaQue(empresa));
-	}
-
-	public List<Empresa> obtenerLista() {
-		return listaEmpresas.stream().map(empresaEnCalculo -> empresaEnCalculo.getEmpresa()).collect(Collectors.toList());
-	}
-
 	private void aplicarComparativas(List<CondicionComparativa> condicionesComparativas, String periodo) {
 		condicionesComparativas.stream().forEach(cond -> aplicarUnicaComparativa(cond,periodo));
 	}
 
 	private void aplicarUnicaComparativa(CondicionComparativa cond, String periodo) {
 		List<Empresa> listaComparada = cond.aplicarCondicion(obtenerLista(), periodo);
-		agregarPesos(listaComparada, cond.getPeso());
+		agregarPesosALaLista(listaComparada, cond.getPeso());
 	}
-
-	private void agregarPesos(List<Empresa> listaComparada, Double pesoDeLaCondicion) {
+	
+	// PESOS
+	private void agregarPesosALaLista(List<Empresa> listaComparada, Double pesoDeLaCondicion) {
 		listaComparada.stream().forEach(empresa -> sumarPeso(getPosicionDeLaEmpresa(listaComparada,empresa), pesoDeLaCondicion, empresa));
 	}
 
@@ -79,5 +65,20 @@ public class AplicaMetodologia {
 
 	private double getPosicionDeLaEmpresa(List<Empresa> listaComparada, Empresa empresa) {
 		return (double) listaComparada.size() - listaComparada.indexOf(empresa);
+	}
+	
+	//AUXILIARES
+	public List<Empresa> obtenerLista() {
+		return listaEmpresas.stream().map(empresaEnCalculo -> empresaEnCalculo.getEmpresa()).collect(Collectors.toList());
+	}
+	
+	private boolean contieneALaEmpresa(Empresa empresa, List<EmpresaEnCalculo> empresasEnCalculo) {
+		return empresasEnCalculo.stream().map(empresaMap -> empresaMap.getEmpresa()).anyMatch(empresaFind -> empresaFind.esLaMismaEmpresaQue(empresa));
+	}
+	
+	private void actualizarLista(List<Empresa> listaFiltrada) {
+		List<EmpresaEnCalculo> empresasEnCalculo = generarEmpresasEnCalculo(listaFiltrada);
+		listaEmpresas = listaEmpresas.stream()
+					 .filter(empresa -> contieneALaEmpresa(empresa.getEmpresa(), empresasEnCalculo)).collect(Collectors.toList());
 	}
 }
