279355dcdaa31558d542cd683078019358bfcf9b
Achus619 <achus619@gmail.com>
2017-09-11 13:44:35 -0300

    el tonto de achus subio mal el persistence

--- src/main/java/domain/indicadores/IndicadorCustom.java
+++ src/main/java/domain/indicadores/IndicadorCustom.java
@@ -1,21 +1,39 @@
 package domain.indicadores;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.uqbar.commons.utils.Observable;
 
 import domain.Empresa;
 import domain.indicadores.calculoIndicadores.Calculable;
 import domain.indicadores.calculoIndicadores.Token;
+import domain.indicadores.calculoIndicadores.ConstructoresIndicador.Analizador;
 
 @Observable
+@Entity
+@Table(name="indicadores")
 public class IndicadorCustom extends Indicador {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column(length=100)
 	public String expresion;
-
+	@Transient
 	public Calculable calculo;
 
 	public IndicadorCustom(String nombre,String expresion,Calculable calculo) {
 		super(nombre);
 		this.expresion = expresion;
 		this.calculo   = calculo;
+	}
+	
+	public IndicadorCustom() {
+		
 	}
 
 	@Override
@@ -40,6 +58,11 @@ public class IndicadorCustom extends Indicador {
 	public void setExpresion(String expresion) {
 		this.expresion = expresion;
 	}
+	
+//	@Column(length=30)
+//	public String getNombrecito() {
+//		return getNombre();
+//	}
 	
 	@Override
 	public boolean esCustom() {
