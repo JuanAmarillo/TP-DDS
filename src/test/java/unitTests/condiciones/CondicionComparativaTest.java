651c4e36b288eac600f7a43b142e77744471ad2b
LPapazian <LPapazian@users.noreply.github.com>
2017-09-26 18:21:41 -0300

    Update CondicionComparativaTest.java

--- src/test/java/unitTests/condiciones/CondicionComparativaTest.java
+++ src/test/java/unitTests/condiciones/CondicionComparativaTest.java
@@ -1,50 +1,75 @@
 package unitTests.condiciones;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 import domain.Empresa;
 import domain.condiciones.CondicionComparativa;
+import domain.condiciones.condicionesPredeterminadas.CEmpresaMayorAntiguedad;
+import domain.condiciones.condicionesPredeterminadas.CEndeudamiento;
+import domain.metodologias.EmpresaConPeso;
+import unitTests.fixtureEmpresas.PreparadorDeEmpresas;
 
-public class CondicionComparativaTest extends CondicionTest<CondicionComparativa> {
+public class CondicionComparativaTest{
+	
+	CondicionComparativa condicionCEndeudamiento = new CEndeudamiento();
+	CondicionComparativa condicionCAntiguedad = new CEmpresaMayorAntiguedad();
+	
+	List<Empresa> listaEmpresas = PreparadorDeEmpresas.prepararEmpresas();
 
-	public void crearCondicion() {
-		condicion = new CondicionComparativa("condicion", mockearIndicador(), mockearOperador());
+	List<EmpresaConPeso> listaEmpresasInicializadas = listaEmpresas.stream().map(empresa -> new EmpresaConPeso(empresa, 0.0))
+			.collect(Collectors.toList());
+	
+	Empresa cocaCola = listaEmpresas.get(0);
+	Empresa sorny = listaEmpresas.get(1);
+	
+	private void imprimirNombres(List<Empresa> listaResultante) {
+		listaResultante.stream().forEach(empresa -> System.out.println(empresa.getNombre()));
+		System.out.println("...");
 	}
-
-	public void evaluarCondicion(Empresa empresaUno, Empresa empresaDos, Integer resultado) {
-		assertEquals(condicion.evaluarCondicionEnPeriodo(empresaUno, empresaDos, "2017"), resultado);
+	
+	private List<Empresa> convertirAEmpresas(List<EmpresaConPeso> listaAConvertir){
+		return listaAConvertir.stream().map(empresaConPeso->empresaConPeso.getEmpresa()).collect(Collectors.toList());
 	}
-
-	@Before
-	public void init() {
-		mockearEmpresas();
-		crearCondicion();
-	}
-
+	
 	@Test
-	public void testEvaluaLaEmpresaUnoContraLaEmpresaDos() {
-		evaluarCondicion(empresaUno, empresaDos, 1);
+	public void testEvaluaPorEndeudamiento(){
+		List<EmpresaConPeso> empresasOrdenadas = condicionCEndeudamiento.aplicarCondicionEnPeriodo(listaEmpresasInicializadas, "pascuas");
+		List<Empresa> empresas = this.convertirAEmpresas(empresasOrdenadas);
+		imprimirNombres(empresas);
+		assertEquals(empresas.get(0), listaEmpresas.get(1));
+		assertEquals(empresas.get(1), listaEmpresas.get(3));
+		assertEquals(empresas.get(2), listaEmpresas.get(0));
+		assertEquals(empresas.get(3), listaEmpresas.get(4));
+		assertEquals(empresas.get(4), listaEmpresas.get(2));
 	}
-
+	
 	@Test
-	public void testEvaluaLaEmpresaDosContraLaEmpresaUno() {
-		evaluarCondicion(empresaDos, empresaUno, -1);
+	public void testEvaluaPorAntiguedad(){
+		List<EmpresaConPeso> empresasOrdenadas = condicionCAntiguedad.aplicarCondicionEnPeriodo(listaEmpresasInicializadas, "pascuas");
+		List<Empresa> empresas = this.convertirAEmpresas(empresasOrdenadas);
+		imprimirNombres(empresas);
+		assertEquals(empresas.get(0), listaEmpresas.get(0));
+		assertEquals(empresas.get(1), listaEmpresas.get(3));
+		assertEquals(empresas.get(2), listaEmpresas.get(2));
+		assertEquals(empresas.get(3), listaEmpresas.get(1));
+		assertEquals(empresas.get(4), listaEmpresas.get(4));
 	}
-
+	
+	
 	@Test
-	public void testAplicarCondicionDesdeEmpresaUnoHastaEmpresaDosCambiaElOrden() {
-		aplicarCondicion(empresaUno, empresaDos);
-		verificarEmpresa(0, empresaDos);
-		verificarEmpresa(1, empresaUno);
+	public void testEvaluaCocaColaYSornyPorEndeudamiento(){
+		Integer resultado = condicionCEndeudamiento.evaluarCondicionEnPeriodo(cocaCola,sorny,"pascuas");
+		assertTrue(resultado>0);
 	}
-
+	
 	@Test
-	public void testAplicarCondicionesDesdeEmpresaDosHastaEmpresaUnoQuedaComoEsta() {
-		aplicarCondicion(empresaDos, empresaUno);
-		verificarEmpresa(0, empresaDos);
-		verificarEmpresa(1, empresaUno);
+	public void testEvaluaCocaColaYSornyPorAntiguedad(){
+		Integer resultado = condicionCAntiguedad.evaluarCondicionEnPeriodo(sorny,cocaCola,"pascuas");
+		assertTrue(resultado>0);
 	}
 }
