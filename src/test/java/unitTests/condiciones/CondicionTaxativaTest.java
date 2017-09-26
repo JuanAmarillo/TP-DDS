8a4a4ecfafae6372bc4cf5a5040ed5b7c20815a0
LPapazian <LPapazian@users.noreply.github.com>
2017-09-26 18:22:07 -0300

    Update CondicionTaxativaTest.java

--- src/test/java/unitTests/condiciones/CondicionTaxativaTest.java
+++ src/test/java/unitTests/condiciones/CondicionTaxativaTest.java
@@ -1,56 +1,63 @@
 package unitTests.condiciones;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 import domain.Empresa;
 import domain.condiciones.CondicionTaxativa;
+import domain.condiciones.condicionesPredeterminadas.TEmpresaMas10Años;
+import domain.condiciones.condicionesPredeterminadas.TEndeudamiento;
+import domain.metodologias.EmpresaConPeso;
+import unitTests.fixtureEmpresas.PreparadorDeEmpresas;
 
-public class CondicionTaxativaTest extends CondicionTest<CondicionTaxativa> {
+public class CondicionTaxativaTest{
 
-	public void crearCondicion() {
-		condicion = new CondicionTaxativa("condicion", mockearIndicador(), mockearOperador(), 15.0);
+	CondicionTaxativa condicionTEmpresaMas10Años = new TEmpresaMas10Años();
+	CondicionTaxativa condicionTEndeudamiento = new TEndeudamiento();
+	
+	List<Empresa> listaEmpresas = PreparadorDeEmpresas.prepararEmpresas();
+
+	List<EmpresaConPeso> listaEmpresasInicializadas = listaEmpresas.stream().map(empresa -> new EmpresaConPeso(empresa, 0.0))
+			.collect(Collectors.toList());
+	
+	Empresa cocaCola = listaEmpresas.get(0);
+	Empresa magnetBox = listaEmpresas.get(2);
+	Empresa pepsiCo = listaEmpresas.get(3);
+	Empresa panaphonics = listaEmpresas.get(4);
+	
+	private void imprimirNombres(List<Empresa> listaResultante) {
+		listaResultante.stream().forEach(empresa -> System.out.println(empresa.getNombre()));
+		System.out.println("...");
 	}
-
-	public void evaluarCondicion(Empresa empresa, Boolean resultado) {
-		assertEquals(condicion.evaluarCondicionEnPeriodo(empresa, "2017"), resultado);
+	
+	private List<Empresa> convertirAEmpresas(List<EmpresaConPeso> listaAConvertir){
+		return listaAConvertir.stream().map(empresaConPeso->empresaConPeso.getEmpresa()).collect(Collectors.toList());
 	}
-
-	@Before
-	public void init() {
-		mockearEmpresas();
-		crearCondicion();
-	}
-
+	
 	@Test
-	public void testLaEmpresaUnoEvaluadaNoPasa() {
-		evaluarCondicion(empresaUno, false);
+	public void testEvaluaPorEmpresaMas10Años(){
+		List<EmpresaConPeso> empresasFiltradas = condicionTEmpresaMas10Años.aplicarCondicionEnPeriodo(listaEmpresasInicializadas, "pascuas");
+		assertEquals(empresasFiltradas.size(),2);
+		List<Empresa> empresas = convertirAEmpresas(empresasFiltradas);
+		imprimirNombres(empresas);
+		assertTrue(empresas.contains(cocaCola));
+		assertTrue(empresas.contains(pepsiCo));
+		
 	}
-
+	
 	@Test
-	public void testLaEmpresaDosEvaluadaPasa() {
-		evaluarCondicion(empresaDos, true);
+	public void testEvaluaPorEndeudamiento(){
+		List<EmpresaConPeso> empresasFiltradas = condicionTEndeudamiento.aplicarCondicionEnPeriodo(listaEmpresasInicializadas, "pascuas");
+		assertEquals(empresasFiltradas.size(),4);
+		List<Empresa> empresas = convertirAEmpresas(empresasFiltradas);
+		imprimirNombres(empresas);
+		assertTrue(empresas.contains(cocaCola));
+		assertTrue(empresas.contains(pepsiCo));
+		assertTrue(empresas.contains(magnetBox));
+		assertTrue(empresas.contains(panaphonics));
 	}
-
-	@Test
-	public void testAplicaLaCondicionALaEmpresaUnoYNoPasa() {
-		aplicarCondicion(empresaUno);
-		verificarTamano(0);
-	}
-
-	@Test
-	public void testAplicaLaCondicionALaEmpresaDosYPasa() {
-		aplicarCondicion(empresaDos);
-		verificarTamano(1);
-	}
-
-	@Test
-	public void testAplicarLaCondicionAAmbasEmpresasYSoloQuedaLaEmpresaDos() {
-		aplicarCondicion(empresaUno, empresaDos);
-		verificarEmpresa(0, empresaDos);
-		verificarTamano(1);
-	}
-
 }
