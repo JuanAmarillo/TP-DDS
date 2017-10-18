1153f1c5b1a5a94f1b44ec4c5c4cb302f58866e3
LPapazian <lilapapazian@msn.com>
2017-10-18 11:03:43 -0300

    me avivaron que condiciones no iba

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -27,7 +27,8 @@ public class Router {
 		Spark.get("/proyecto/indicadores", IndicadoresController::get, engine);
 		Spark.get("/proyecto/indicadores/new", IndicadoresController::nuevo, engine);
 		Spark.post("/proyecto/indicadores/new", IndicadoresController::agregar, engine);
-		Spark.get("/proyecto/condiciones/new", CondicionesController::nuevo, engine);
+		Spark.get("/proyecto/condicionComparativa/new", CondicionComparativaController::nuevo, engine);
+		Spark.get("/proyecto/condicionTaxativa/new", CondicionTaxativaController::nuevo, engine);
 		Spark.get("/proyecto/metodologias", MetodologiasController::get, engine);
 		Spark.after(FiltersController::after);
 
