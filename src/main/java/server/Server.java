17259d25c60e1ab2534bb176fa570a64e2d90da2
padillajuan <juanpadilla.jip@gmail.com>
2017-10-28 22:19:20 -0300

    I derped. saco lo viejo de UI. Queda en la branch cargaDeData

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,6 +1,5 @@
 package server;
 
-import application.InversionesApp;
 import domain.login.Usuario;
 import domain.repositorios.RepositorioCondiciones;
 import domain.repositorios.RepositorioIndicadores;
@@ -11,15 +10,9 @@ import spark.debug.DebugScreen;
 
 public class Server {
 	public static void main(String[] args) {
-		/*TransactionManager.instance().crearTransaccion();
-		RepositorioUsuarios.instance().agregar(new Usuario("pepito", "login", "password"));
-		TransactionManager.instance().cerrarTransaccion();
-		RepositorioIndicadores.instance();
-		RepositorioCondiciones.instance();*/
 		Spark.port(9000);
 		DebugScreen.enableDebugScreen();
 		Router.configure();
-		new InversionesApp().start();
 	}
 
 }
