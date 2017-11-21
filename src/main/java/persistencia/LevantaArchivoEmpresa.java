d8bfc4a88ca3f0fe31c60090b2d3faf1ecc5f2af
Juan <juan.alb.amarillo@gmail.com>
2017-11-21 01:23:49 -0300

    saco el for

--- src/main/java/persistencia/LevantaArchivoEmpresa.java
+++ src/main/java/persistencia/LevantaArchivoEmpresa.java
@@ -1,46 +1,53 @@
 package persistencia;
 
 import java.io.File;
-import static java.nio.file.StandardCopyOption.*;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.*;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
-import com.google.common.io.Files;
-
 import domain.*;
 import domain.repositorios.RepositorioEmpresas;
 
+public class LevantaArchivoEmpresa {
 
-public class LevantaArchivoEmpresa{
-	
 	private String sinProcesar; // Representa la direccion de la carpeta donde estan albergados los archivos
-	private String procesadosCorrectamente; 
-	private String procesadosErroneos; 
+	private String procesadosCorrectamente;
+	private String procesadosErroneos;
 	private final static Logger LOGGER = Logger.getLogger("Carga de archivos");
-	
+
 	public LevantaArchivoEmpresa(String sinProcesar, String procesadosCorrectamente, String procesadosErroneos) {
-		super();
 		this.sinProcesar = sinProcesar;
 		this.procesadosCorrectamente = procesadosCorrectamente;
 		this.procesadosErroneos = procesadosErroneos;
 	}
-	
+
 	public void execute() {
-		File[] carpetaDeEmpresas;
-		carpetaDeEmpresas = new File(sinProcesar).listFiles();
-		for(File archivoDeLaEmpresa : carpetaDeEmpresas) {
-			try {
-				cargarArchivo(archivoDeLaEmpresa);
-				moverArchivoCargaCorrecta(archivoDeLaEmpresa);
-				LOGGER.log(Level.INFO, "Se cargo correctamente el archivo " + archivoDeLaEmpresa.getAbsolutePath()) ;
-			}
-			catch (IOException e) {
-				moverArchivoCargaIncorrecta(archivoDeLaEmpresa);
-				LOGGER.log(Level.WARNING, e.toString(), e);
-			}
+		Arrays.asList(empresasSinProcesar()).stream().forEach(empresa -> cargarArchivo(empresa));
+	}
+
+	private File[] empresasSinProcesar() {
+		return new File(sinProcesar).listFiles();
+	}
+
+	private void cargarArchivo(File empresa) {
+		try {
+			cargaExitosa(empresa);
+		} catch (IOException e) {
+			cargaFallida(empresa, e);
 		}
+	}
+
+	private void cargaFallida(File archivoDeLaEmpresa, IOException e) {
+		moverArchivoCargaIncorrecta(archivoDeLaEmpresa);
+		LOGGER.log(Level.WARNING, e.toString(), e);
+	}
+
+	private void cargaExitosa(File archivoDeLaEmpresa) throws IOException {
+		agregarAlRepositorio(archivoDeLaEmpresa);
+		moverArchivoCargaCorrecta(archivoDeLaEmpresa);
+		LOGGER.log(Level.INFO, "Se cargo correctamente el archivo " + archivoDeLaEmpresa.getAbsolutePath());
 	}
 
 	public void moverArchivoCargaIncorrecta(File archivoDeLaEmpresa) {
@@ -50,21 +57,21 @@ public class LevantaArchivoEmpresa{
 	public void moverArchivoCargaCorrecta(File archivoDeLaEmpresa) {
 		moveFile(archivoDeLaEmpresa, procesadosCorrectamente + archivoDeLaEmpresa.getName());
 	}
-	
+
 	public void moveFile(File archivoDeLaEmpresa, String destino) {
 		archivoDeLaEmpresa.renameTo(new File(destino));
 	}
 
 	public Empresa getEmpresaDelArchivo(File archivoDeLaEmpresa) throws IOException {
-		return new ObjectMapper().readValue(archivoDeLaEmpresa,Empresa.class);
+		return new ObjectMapper().readValue(archivoDeLaEmpresa, Empresa.class);
 	}
-	
-	public void cargarArchivo(File archivoDeLaEmpresa) throws IOException {
+
+	public void agregarAlRepositorio(File archivoDeLaEmpresa) throws IOException {
 		RepositorioEmpresas.instance().agregar(getEmpresaDelArchivo(archivoDeLaEmpresa));
 	}
 
-	public void setFilepath(String filePath){
+	public void setFilepath(String filePath) {
 		this.sinProcesar = filePath;
 	}
-	
+
 }
